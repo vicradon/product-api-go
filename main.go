937ef4e27cb886a9eacb4bf254378c25da4a7dcf
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2024-10-21 09:28:41 +0100

    chore: hanlde product create route

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-    "strings"
+	"strings"
 )
 
 type Product struct {
@@ -13,11 +13,11 @@ type Product struct {
 }
 
 func getItems(w http.ResponseWriter, r *http.Request, id int) {
-    if id != 0 {
-     	fmt.Fprintf(w, "Got item, %d", id)
-    } else {
-       fmt.Fprintf(w, "Fetched all items")
-    }
+	if id != 0 {
+		fmt.Fprintf(w, "Got item, %d", id)
+	} else {
+		fmt.Fprintf(w, "Fetched all items")
+	}
 }
 
 func createItem(w http.ResponseWriter, r *http.Request) {
@@ -33,24 +33,33 @@ func deleteItem(w http.ResponseWriter, r *http.Request, id int) {
 }
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "API docs") 
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "API docs")
+	})
+
+	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			createItem(w, r)
+		default:
+			http.Redirect(w, r, "/products/", http.StatusMovedPermanently)
+		}
+	})
 
 	http.HandleFunc("/products/", func(w http.ResponseWriter, r *http.Request) {
-        path := strings.TrimPrefix(r.URL.Path, "/products")
-        path = strings.Trim(path, "/")
-     
-        var id int
-        var err error
+		path := strings.TrimPrefix(r.URL.Path, "/products")
+		path = strings.Trim(path, "/")
 
-        if path != "" {
-           id, err = strconv.Atoi(path)
-           if err != nil {
-              http.Error(w, "Invalid item Id", http.StatusBadRequest)
-              return 
-           }
-        }
+		var id int
+		var err error
+
+		if path != "" {
+			id, err = strconv.Atoi(path)
+			if err != nil {
+				http.Error(w, "Invalid item Id", http.StatusBadRequest)
+				return
+			}
+		}
 
 		switch r.Method {
 		case http.MethodPost:
